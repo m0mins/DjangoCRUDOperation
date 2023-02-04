6f0726aeea7cc0cfea9ba43e13c4080c5d05c658
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-02-04 09:01:30 +0600

    postgres connect

--- djangocrudproj/settings.py
+++ djangocrudproj/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'djangocrudproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'DjangoCrudDB',
+      'USER':'postgres',
+      'PASSWORD':'123456',
+      'HOST':'127.0.0.1',
+      'PORT':'5432',
+   }
+
 }
 
 
