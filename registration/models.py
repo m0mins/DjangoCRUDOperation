1e65f2fd303061fbff618423ade42e7e5caa6979
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-02-04 09:16:48 +0600

    position and employee model created

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    title=models.CharField(max_length=25)
+class Employee(models.Model):
+    fullname=models.CharField(max_length=50)
+    emp_code=models.CharField(max_length=10)
+    mobile=models.CharField(max_length=15)
+    position=models.ForeignKey(Position,on_delete=models.CASCADE)
