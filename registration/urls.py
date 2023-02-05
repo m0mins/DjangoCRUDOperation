1f36226b11f0f489dfe75bb79adbd9378fdd0cde
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-02-05 09:13:42 +0600

    Data insert and details

--- registration/urls.py
+++ registration/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns=[
     path('index/',views.index,name='index'),
     path('',views.employee_form, name='employee_form'),
+    path('list/', views.employee_list, name='employee_list'),
 
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . forms import EmployeeForm
+from .models import Employee
 
 def index(request):
     return render(request,'index.html')
 
 def employee_list(request):
-    return render(request,'employee_list.html')
+    context={'employee_list':Employee.objects.all()}
+
+    return render(request,'employee_list.html',context)
 def employee_form(request):
-    form=EmployeeForm()
-    return render(request,'employee_form.html',{'form':form})
+    if request.method=='GET':
+        form=EmployeeForm()
+        return render(request,'employee_form.html',{'form':form})
+    else:
+        form=EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/list')
+
 def employee_delete(request):
     return
